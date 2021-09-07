dfd0b37232c9813b3ed1829294ba6232630c834b
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-09-07 17:01:52 -0300

    Adicionando metodo de saque em CC

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
 
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.saldo = 100;
+contaCorrenteRicardo.sacar(200);
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(50);
+console.log(contaCorrenteRicardo.saldo);
 
 const cliente2 = new Cliente();
 
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+
+const contaCorrenteAlice = new ContaCorrente();
+contaCorrenteAlice.saldo = 0;
+contaCorrenteAlice.agencia = 1002;
 
 console.log(cliente1);
 console.log(cliente2);
