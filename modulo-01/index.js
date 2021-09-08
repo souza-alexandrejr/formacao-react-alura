680cb3b2e7b7e09f961141d7d8446584557d5dcf
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-09-08 11:50:43 -0300

    Melhorando organização do código

--- modulo-01/index.js
+++ modulo-01/index.js
@@ -1,32 +1,3 @@
-class Cliente {
-    nome;
-    cpf;
-}
-
-class ContaCorrente {
-    agencia;
-    // proposta de campos privados usando '#':
-    // https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0; // convenção adotada pela comunidade dev (não é de fato privado)
-
-    depositar(valor) {
-        if (valor > 0) {
-            this._saldo += valor;
-            console.log("Depósito de " + valor + " realizado com sucesso!");
-            console.log("Novo saldo: " + this._saldo);
-        }
-    }
-
-    sacar(valor) {
-        if (valor < 0) {
-            return; // early return (interrompe a execução do método)
-        }
-        this._saldo -= valor;
-        console.log("Saque de " + valor + " realizado com sucesso!");
-        console.log("Novo saldo: " + this._saldo);
-    }
-}
-
 const cliente1 = new Cliente();
 
 cliente1.nome = "Ricardo";
