29c9f3afff3c3ba534fb2d392cb2787301874037
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-09-12 17:27:19 -0300

    Arrow functions: retornando um TypeError

---
+++ modulo-01/js-arrow-functions/index.js
@@ -0,0 +1,17 @@
+class ModuloDeImpressao {
+    constructor() {
+        this._codigo = 10;
+    }
+
+    // retorno TypeError pois this._codigo não faz parte
+    // do contexto da função passada no forEach
+    imprime(nomes) {
+        nomes.forEach(function(nome){
+        console.log(`${this._codigo}: ${nome}`);
+     });
+   }
+}
+
+const professores = ['Elias', 'Yuri', 'Gabriel', 'Guilherme', 'Yan'];
+const impressao = new ModuloDeImpressao();
+impressao.imprime(professores);
