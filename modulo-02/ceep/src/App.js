afb34b799f83a71ce1904df5cf0a844674c1960d
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-09-13 14:27:32 -0300

    Criando o primeiro componente

--- modulo-02/ceep/src/App.js
+++ modulo-02/ceep/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ListaDeNotas } from "./components/ListaDeNotas";
 
 function App() {
   return (
@@ -8,32 +9,7 @@ function App() {
         <textarea placeholder="Escreva sua nota" />
         <button>Criar Nota</button>
       </form>
-      <ul>
-        <li>
-          <section>
-            <header>
-              <h3>Título #1</h3>
-            </header>
-            <p>Nota #1</p>
-          </section>
-        </li>
-        <li>
-          <section>
-            <header>
-              <h3>Título #2</h3>
-            </header>
-            <p>Nota #2</p>
-          </section>
-        </li>
-        <li>
-          <section>
-            <header>
-              <h3>Título #3</h3>
-            </header>
-            <p>Nota #3</p>
-          </section>
-        </li>
-      </ul>
+      <ListaDeNotas />
     </section>   
   );
 }
---
+++ modulo-02/ceep/src/components/ListaDeNotas.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+
+export class ListaDeNotas extends Component {
+    render() {
+        return (
+            <ul>
+                <li>
+                    <section>
+                    <header>
+                        <h3>Título #1</h3>
+                    </header>
+                    <p>Nota #1</p>
+                    </section>
+                </li>
+                <li>
+                    <section>
+                    <header>
+                        <h3>Título #2</h3>
+                    </header>
+                    <p>Nota #2</p>
+                    </section>
+                </li>
+                <li>
+                    <section>
+                    <header>
+                        <h3>Título #3</h3>
+                    </header>
+                    <p>Nota #3</p>
+                    </section>
+                </li>
+            </ul> 
+        );
+    }
+}
