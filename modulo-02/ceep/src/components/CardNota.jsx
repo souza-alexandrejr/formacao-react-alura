7aaa2f96100a9d2442aecfb7eb670ad096e4387e
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-09-13 14:46:38 -0300

    Criando o componente CardNota com React Simple Snippets plugin

---
+++ modulo-02/ceep/src/components/CardNota.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class CardNota extends Component {
+    render() { 
+        return (
+            <section>
+                <header>
+                    <h3>Título #1</h3>
+                </header>
+                <p>Nota #1</p>
+            </section>
+        );
+    }
+}
+ 
+export default CardNota;
--- modulo-02/ceep/src/components/ListaDeNotas.jsx
+++ modulo-02/ceep/src/components/ListaDeNotas.jsx
@@ -1,32 +1,18 @@
 import React, { Component } from "react";
+import CardNota from "./CardNota";
 
 class ListaDeNotas extends Component {
     render() {
         return (
             <ul>
                 <li>
-                    <section>
-                    <header>
-                        <h3>Título #1</h3>
-                    </header>
-                    <p>Nota #1</p>
-                    </section>
+                    <CardNota />
                 </li>
                 <li>
-                    <section>
-                    <header>
-                        <h3>Título #2</h3>
-                    </header>
-                    <p>Nota #2</p>
-                    </section>
+                    <CardNota />
                 </li>
                 <li>
-                    <section>
-                    <header>
-                        <h3>Título #3</h3>
-                    </header>
-                    <p>Nota #3</p>
-                    </section>
+                    <CardNota />
                 </li>
             </ul> 
         );
