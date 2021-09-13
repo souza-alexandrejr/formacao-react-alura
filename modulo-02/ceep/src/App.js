f40fb6d629f4ee0720a4607780a1b4874c53895f
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-09-13 14:16:04 -0300

    Renderizando uma section com JSX

--- modulo-02/ceep/src/App.js
+++ modulo-02/ceep/src/App.js
@@ -2,7 +2,39 @@ import React from "react";
 
 function App() {
   return (
-    <h1>Meu primeiro app React.</h1>
+    <section>
+      <form>
+        <input type="text" placeholder="Título"/>
+        <textarea placeholder="Escreva sua nota" />
+        <button>Criar Nota</button>
+      </form>
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>Título #1</h3>
+            </header>
+            <p>Nota #1</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Título #2</h3>
+            </header>
+            <p>Nota #2</p>
+          </section>
+        </li>
+        <li>
+          <section>
+            <header>
+              <h3>Título #3</h3>
+            </header>
+            <p>Nota #3</p>
+          </section>
+        </li>
+      </ul>
+    </section>   
   );
 }
 
