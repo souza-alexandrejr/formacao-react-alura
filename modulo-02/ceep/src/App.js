b234261d936ad0db36a8a035e2176ae632d94541
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-09-15 11:41:37 -0300

    Atualizando o state do componente App

--- modulo-02/ceep/src/App.js
+++ modulo-02/ceep/src/App.js
@@ -7,19 +7,25 @@ import "./assets/index.css";
 class App extends Component {
   constructor(props) {
     super();
-    this.notas = [];
+    this.state = {
+      notas:[]
+    };
   }
 
   criarNota(titulo, texto) {
     const novaNota = {titulo, texto};
-    this.notas.push(novaNota);
+    const novoArrayDeNotas = [...this.state.notas, novaNota]
+    const novoEstado = {
+      notas:novoArrayDeNotas
+    }
+    this.setState(novoEstado);
   }
 
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.notas}/>
+        <ListaDeNotas notas={this.state.notas}/>
       </section>   
     );
   }
