3b4eaa5ae1dab7cf5d5fbdfbe2c36dd40b3154f0
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-09-16 14:24:57 -0300

    Adicionando estilo css na lista de categorias

--- modulo-02/ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ modulo-02/ceep/src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
+import "./estilo.css";
 
 class ListaDeCategorias extends Component {
     render() { 
         return (
-            <section>
-                <ul>
-                    <li>Categoria 1</li>
-                    <li>Categoria 2</li>
-                    <li>Categoria 3</li>
+            <section className="lista-categorias">
+                <ul className="lista-categorias_lista">
+                    <li className="lista-categorias_item">Categoria 1</li>
+                    <li className="lista-categorias_item">Categoria 2</li>
+                    <li className="lista-categorias_item">Categoria 3</li>
                 </ul>
-                <input type="text"></input>
+                <input 
+                    type="text"
+                    className="lista-categorias_input"
+                    placeholder="Adicionar Categoria" 
+                />
             </section>
         );
     }
