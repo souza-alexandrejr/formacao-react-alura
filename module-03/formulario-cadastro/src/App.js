a0e0f3adaa5d7cd6b1edc99449a5c4c2f1a1eec6
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-09-18 20:21:19 -0300

    Formatando código com Prettier plugin

--- module-03/formulario-cadastro/src/App.js
+++ module-03/formulario-cadastro/src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import './App.css';
-import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro';
-import { Typography, Container } from '@mui/material';
-import '@fontsource/roboto/300.css';
+import React from "react";
+import "./App.css";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
+import { Typography, Container } from "@mui/material";
+import "@fontsource/roboto/300.css";
 
 function App() {
   return (
     <Container maxWidth="sm" component="article">
-      <Typography variant="h3" component="h1" align="center"> Formulário de Cadastro </Typography>
-      <FormularioCadastro /> 
-    </Container>    
+      <Typography variant="h3" component="h1" align="center">
+        {"Formulário de Cadastro"}
+      </Typography>
+      <FormularioCadastro />
+    </Container>
   );
 }
 
--- module-03/formulario-cadastro/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ module-03/formulario-cadastro/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
-import { Button, TextField, FormControlLabel, Switch } from '@mui/material';
+import React from "react";
+import { Button, TextField, FormControlLabel, Switch } from "@mui/material";
 
 function FormularioCadastro() {
-    return (
-        <form>
-            <TextField id="nome" label="Nome" variant="outlined" margin="normal" fullWidth />
-            <TextField id="sobrenome" label="Sobrenome" variant="outlined" margin="normal" fullWidth />
-            <TextField id="cpf" label="CPF" variant="outlined" margin="normal" fullWidth />
+  return (
+    <form>
+      <TextField
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
 
-            <FormControlLabel control={<Switch defaultChecked />} label="Promoções" />
-            <FormControlLabel control={<Switch defaultChecked />} label="Novidades" />
+      <FormControlLabel control={<Switch defaultChecked />} label="Promoções" />
+      <FormControlLabel control={<Switch defaultChecked />} label="Novidades" />
 
-            <Button variant="contained" type="submit">
-                Cadastrar
-            </Button>
-        </form>
-    ); 
+      <Button variant="contained" type="submit">
+        Cadastrar
+      </Button>
+    </form>
+  );
 }
 
 export default FormularioCadastro;
