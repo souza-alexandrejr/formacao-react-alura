5584dbcd1ba8a2cd0ffa95ec140704affe17ee6b
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-09-18 18:38:56 -0300

    Importando pacote MaterialUI
    Importando pacote MaterialUI

--- module-03/formulario-cadastro/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ module-03/formulario-cadastro/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function FormularioCadastro() {
     return (
         <form>
-            <input type="text" />
+            <TextField id="nome" label="Nome" variant="outlined" fullWidth="true" />
+            <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth="true" />
+            <TextField id="cpf" label="CPF" variant="outlined" fullWidth="true" />
+
+            <label>Promoções</label>
+            <input type="checkbox" />
+
+            <label>Novidades</label>
+            <input type="checkbox" />
+
+            <Button variant="contained" type="submit">
+                Cadastrar
+            </Button>
         </form>
     ); 
 }
