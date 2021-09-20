31d79d2a45c63658da8596249854b0edc3c26a00
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-09-20 11:18:03 -0300

    Criando um form controlado com Hooks - useState

--- modulo-03/formulario-cadastro/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ modulo-03/formulario-cadastro/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextField, FormControlLabel, Switch } from "@mui/material";
 
 function FormularioCadastro() {
+  const [nome, setNome] = useState("Ricardo");
+
   return (
-    <form>
+    <form onSubmit={
+      (event) => {
+        event.preventDefault();
+        console.log(nome);
+      }
+    }>
       <TextField
         id="nome"
         label="Nome"
         variant="outlined"
         margin="normal"
         fullWidth
+        value={nome}
+        onChange={
+          (event) => {
+            setNome(event.target.value);
+            if (nome.length >= 3) {
+              setNome(nome.substr(0,3));
+            }
+          }
+        }
       />
       <TextField
         id="sobrenome"
