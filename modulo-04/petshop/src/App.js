b177370c06267901364d91bee039819e697d6769
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-09-23 14:50:15 -0300

    Definindo rotas com IFs

--- modulo-04/petshop/src/App.js
+++ modulo-04/petshop/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import { Fragment } from 'react';
+import './assets/css/base/base.css'
 import Home from './paginas/Home';
 import Sobre from './paginas/Sobre';
 
 function App() {
+  const Router = () => {
+    const location = window.location.pathname;
+    if (location === "/sobre") {
+      return <Sobre />
+    } else {
+      return <Home />
+    }
+  }
+
   return (
     <Fragment>
-      <Home />
-      <Sobre />
+      { Router() }
     </Fragment>
   );
 }
