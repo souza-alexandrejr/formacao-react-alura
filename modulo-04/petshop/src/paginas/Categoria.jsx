09d1b6a2932b815c15099d4e02e34dd24847e25c
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-10-07 09:47:11 -0300

    Exibindo subcategorias

--- modulo-04/petshop/src/paginas/Categoria.jsx
+++ modulo-04/petshop/src/paginas/Categoria.jsx
@@ -1,12 +1,22 @@
-import React from "react";
-import { Route, useParams, useRouteMatch } from "react-router";
+import React, { useEffect, useState } from "react";
+import { Route, Switch, useParams, useRouteMatch } from "react-router";
+import { Link } from "react-router-dom";
+import { busca } from "../api/api";
 import "../assets/css/blog.css";
 import ListaCategorias from "../componentes/ListaCategorias";
 import ListaPost from "../componentes/ListaPost";
+import SubCategoria from "./SubCategoria";
 
 const Categoria = () => {
   const { id } = useParams();
-  const { path } = useRouteMatch();
+  const { url, path } = useRouteMatch();
+  const [subcategorias, setSubcategorias] = useState([]);
+
+  useEffect(() => {
+    busca(`/categorias/${id}`, (categoria) => {
+      setSubcategorias(categoria.subcategorias);
+    });
+  }, [id]);
 
   return (
     <>
@@ -15,9 +25,29 @@ const Categoria = () => {
       </div>
 
       <ListaCategorias />
-      <Route exact path={`${path}/`}>
-        <ListaPost url={`/posts?categoria=${id}`} />
-      </Route>
+
+      <ul className="lista-categorias container flex">
+        {subcategorias.map((subcategoria) => {
+          return (
+            <li
+              className={`lista-categorias__categoria lista-categorias__categoria--${id}`}
+              key={subcategoria}
+            >
+              <Link to={`${url}/${subcategoria}`}>{subcategoria}</Link>
+            </li>
+          );
+        })}
+      </ul>
+
+      <Switch>
+        <Route exact path={`${path}/`}>
+          <ListaPost url={`/posts?categoria=${id}`} />
+        </Route>
+
+        <Route path={`${path}/:subcategoria`}>
+          <SubCategoria />
+        </Route>
+      </Switch>
     </>
   );
 };
---
+++ modulo-04/petshop/src/paginas/SubCategoria.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useParams } from "react-router";
+import ListaPost from "../componentes/ListaPost";
+
+const SubCategoria = () => {
+  const { subcategoria } = useParams();
+
+  return <ListaPost url={`/posts?subcategoria=${subcategoria}`} />;
+};
+
+export default SubCategoria;
