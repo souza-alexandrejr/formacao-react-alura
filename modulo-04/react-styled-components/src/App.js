357aadffdf36254cdeb0a7d0d0ffa53da7a43301
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-10-07 18:16:52 -0300

    Criando o componente GlobalStyle

--- modulo-04/react-styled-components/src/App.js
+++ modulo-04/react-styled-components/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
+import GlobalStyle from "./Components/GlobalStyle";
 
 function App() {
   return (
     <>
-      <Cabecalho />      
+      <GlobalStyle />
+      <Cabecalho />
       <Container />
     </>
   );
