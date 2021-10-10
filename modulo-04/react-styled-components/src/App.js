6ff264eb284683d8312998626723bf535586bcf9
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-10-10 20:10:28 -0300

    Adicionando o botão de troca de tema

--- modulo-04/react-styled-components/src/App.js
+++ modulo-04/react-styled-components/src/App.js
@@ -6,11 +6,24 @@ import GlobalStyle from "./Components/GlobalStyle";
 
 import { ThemeProvider } from "styled-components";
 import { temaClaro, temaEscuro } from "./Components/ui/temas";
+import { BtnTema } from "./Components/ui";
+import SwitcherTema from "./Components/SwitcherTema";
+
+import { useState } from "react";
 
 function App() {
+  const [tema, setTema] = useState(true);
+
+  const trocarTema = () => {
+    setTema((tema) => !tema);
+  }
+
   return (
-    <ThemeProvider theme={temaEscuro}>
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
       <GlobalStyle />
+      <BtnTema onClick={trocarTema}>
+        <SwitcherTema tema={tema} />
+      </BtnTema>
       <Cabecalho />
       <Container />
     </ThemeProvider>
