b101ce88fc062a908f74aa4eb03e80b9d9f9fb41
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-10-10 19:37:38 -0300

    Adicionando ícones aos itens do extrato

--- modulo-04/react-styled-components/src/Components/Itens/index.jsx
+++ modulo-04/react-styled-components/src/Components/Itens/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Item from "../Item";
+import ImageFilter from "../ImageFilter";
 
 const Itens = styled.div`
   box-shadow: 4px 4px 20px 0px rgba(0, 0, 0, 0.1);
@@ -16,6 +17,7 @@ const Itens = styled.div`
 export default (props) => {
   return (
     <Itens>
+      {ImageFilter(props.type)}
       <Item {...props} />
       <span>{props.date}</span>
     </Itens>
