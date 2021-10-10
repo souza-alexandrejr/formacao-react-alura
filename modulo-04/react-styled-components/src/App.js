1edbac818fd5071e4a949712fc0facb2963bff90
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-10-10 19:56:35 -0300

    Criando o tema escuro

--- modulo-04/react-styled-components/src/App.js
+++ modulo-04/react-styled-components/src/App.js
@@ -4,13 +4,16 @@ import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import GlobalStyle from "./Components/GlobalStyle";
 
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaEscuro } from "./Components/ui/temas";
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaEscuro}>
       <GlobalStyle />
       <Cabecalho />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
