b3cc9dbcc63b62a25fff56657ac64657ce9f7590
Alexandre de Souza Jr <83607914+alexandresjunior@users.noreply.github.com>
2021-10-10 19:24:14 -0300

    Adicionando o componente Item

--- modulo-04/react-styled-components/src/Components/Extrato/index.jsx
+++ modulo-04/react-styled-components/src/Components/Extrato/index.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React from "react";
 import { Box, Botao } from "../../Components/ui";
-import { extratoLista } from '../../info';
+import { extratoLista } from "../../info";
+import Itens from "../Itens";
 
 const Extrato = () => {
-    return <Box>
-        {
-            extratoLista.updates.map(({id, type, from, value, date}) => {
-                return (
-                    <div key={id}>
-                        <div>{type}</div>
-                        <div>{from}</div>
-                    </div>
-                )
-            })
-        }
-        <Botao>Ver Mais</Botao>
+  return (
+    <Box>
+      {extratoLista.updates.map(({ id, type, from, value, date }) => {
+        return (
+          <Itens
+            key={id}
+            type={type}
+            from={from}
+            value={value}
+            date={date}
+          ></Itens>
+        );
+      })}
+      <Botao>Ver Mais</Botao>
     </Box>
-}
+  );
+};
 
 export default Extrato;
